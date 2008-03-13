ab5f70afdfac927a6bdd40030df7bbf08138602c
singh <singh@febdc8c4-1326-0410-abd6-27cb15717650>
2008-03-13 12:50:44 +0000

    Added select() support.

--- fuse_kludges.h
+++ fuse_kludges.h
@@ -13,4 +13,20 @@
 #include <sys/mount.h>
 #include <sys/types.h>
 
+#if M_MACFUSE_ENABLE_DSELECT
+
+struct fuse_selinfo {
+    unsigned char __data[32];
+};
+
+#define POLLIN          0x0001          /* any readable data available */
+#define POLLPRI         0x0002          /* OOB/Urgent readable data */
+#define POLLOUT         0x0004          /* file descriptor is writeable */
+#define POLLRDNORM      0x0040          /* non-OOB/URG data available */
+#define POLLWRNORM      POLLOUT         /* no write type differentiation */
+#define POLLRDBAND      0x0080          /* OOB/Urgent readable data */
+#define POLLWRBAND      0x0100          /* OOB/Urgent data can be written */
+
+#endif /* M_MACFUSE_ENABLE_DSELECT */
+
 #endif /* _FUSE_KLUDGES_H_ */
