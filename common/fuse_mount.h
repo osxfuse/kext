74bd5a9609e486a84cec18146e30ae3de9151b74
singh <singh@febdc8c4-1326-0410-abd6-27cb15717650>
2008-11-27 06:30:27 +0000

    10.5: Bits for sparse.

--- common/fuse_mount.h
+++ common/fuse_mount.h
@@ -90,6 +90,7 @@ enum {
 #define FUSE_MOPT_PING_DISKARB           0x0000000400000000ULL
 #define FUSE_MOPT_AUTO_CACHE             0x0000000800000000ULL
 #define FUSE_MOPT_NATIVE_XATTR           0x0000001000000000ULL
+#define FUSE_MOPT_SPARSE                 0x0000002000000000ULL
 
 #define FUSE_MAKEDEV(x, y)              ((dev_t)(((x) << 24) | (y)))
 #define FUSE_MINOR_MASK                 0x00FFFFFFUL
