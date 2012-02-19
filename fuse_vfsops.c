2b3d3b28a028d94def69efa34887d9df1f6cf247
Benjamin Fleischer <fleiben@gmail.com>
2012-02-19 05:17:27 +0100

    Enable capability VOL_CAP_FMT_FAST_STATFS
    This is done to make Finder display updated statfs information (i.e. free/used
    space on a volume) if files are edited or created/removed on Mac OS X 10.6 and
    later.

--- fuse_vfsops.c
+++ fuse_vfsops.c
@@ -822,7 +822,7 @@ handle_capabilities_and_attributes(mount_t mp, struct vfs_attr *attr)
 //      | VOL_CAP_FMT_ZERO_RUNS
 //      | VOL_CAP_FMT_CASE_SENSITIVE
         | VOL_CAP_FMT_CASE_PRESERVING
-//      | VOL_CAP_FMT_FAST_STATFS
+        | VOL_CAP_FMT_FAST_STATFS
         | VOL_CAP_FMT_2TB_FILESIZE
 //      | VOL_CAP_FMT_OPENDENYMODES
 //      | VOL_CAP_FMT_HIDDEN_FILES
