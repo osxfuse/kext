osxfuse/kext

commit 0a079be6e9d191de985c9c0a194752f56cc48070

(nodelock, biglock) Log thread that acquires/releases the lock

fuse_biglock_vnops.h
@@ -14,56 +14,63 @@
 #endif
 
 #if M_OSXFUSE_ENABLE_INTERIM_FSNODE_LOCK
+#include <kern/thread.h>
 
 #define fuse_nodelock_lock(node, type) \
 	do { \
+		thread_t nodelock_thread = current_thread(); \
 		int fnl_err; \
-		log("%s: Locking node %p...", __FUNCTION__, node); \
+		log("%s thread=%p: Locking node %p...", __FUNCTION__, nodelock_thread, node); \
 		fnl_err = fusefs_lock(node, type); \
 		if(fnl_err) \
 			return fnl_err; \
-		log("%s:   node %p locked!", __FUNCTION__, node); \
+		log("%s thread=%p: node %p locked!", __FUNCTION__, nodelock_thread, node); \
 	} while(0)
 
 #define fuse_nodelock_unlock(node) \
 	do { \
-		log("%s: Unlocking node %p...", __FUNCTION__, node); \
+		thread_t nodelock_thread = current_thread(); \
+		log("%s thread=%p: Unlocking node %p...", __FUNCTION__, nodelock_thread, node); \
 		fusefs_unlock(node); \
-		log("%s:   node %p unlocked!", __FUNCTION__, node); \
+		log("%s thread=%p: node %p unlocked!", __FUNCTION__, nodelock_thread, node); \
 	} while(0)
 
 #define fuse_nodelock_lock_pair(node1, node2, type) \
 	do { \
+		thread_t nodelock_thread = current_thread(); \
 		int fnl_err; \
-		log("%s: Locking node pair (%p, %p)...", __FUNCTION__, node1, node2); \
+		log("%s thread=%p: Locking node pair (%p, %p)...", __FUNCTION__, nodelock_thread, node1, node2); \
 		fnl_err = fusefs_lockpair(node1, node2, type); \
 		if(fnl_err) \
 			return fnl_err; \
-		log("%s:   node pair (%p, %p) locked!", __FUNCTION__, node1, node2); \
+		log("%s thread=%p: node pair (%p, %p) locked!", __FUNCTION__, nodelock_thread, node1, node2); \
 	} while(0)
 
 #define fuse_nodelock_unlock_pair(node1, node2) \
 	do { \
-		log("%s: Unlocking node pair (%p, %p)...", __FUNCTION__, node1, node2); \
+		thread_t nodelock_thread = current_thread(); \
+		log("%s thread=%p: Unlocking node pair (%p, %p)...", __FUNCTION__, nodelock_thread, node1, node2); \
 		fusefs_unlockpair(node1, node2); \
-		log("%s:   node pair (%p, %p) unlocked!", __FUNCTION__, node1, node2); \
+		log("%s thread=%p: node pair (%p, %p) unlocked!", __FUNCTION__, nodelock_thread, node1, node2); \
 	} while(0)
 
 #define fuse_nodelock_lock_four(node1, node2, node3, node4, type) \
 	do { \
+		thread_t nodelock_thread = current_thread(); \
 		int fnl_err; \
-		log("%s: Locking node pair (%p, %p, %p, %p)...", __FUNCTION__, node1, node2, node3, node4); \
+		log("%s thread=%p: Locking node pair (%p, %p, %p, %p)...", __FUNCTION__, nodelock_thread, node1, node2, node3, node4); \
 		fnl_err = fusefs_lockfour(node1, node2, node3, node4, type); \
 		if(fnl_err) \
 			return fnl_err; \
-		log("%s:   node pair (%p, %p, %p, %p) locked!", __FUNCTION__, node1, node2, node3, node4); \
+		log("%s thread=%p: node pair (%p, %p, %p, %p) locked!", __FUNCTION__, nodelock_thread, node1, node2, node3, node4); \
 	} while(0)
 
 #define fuse_nodelock_unlock_four(node1, node2, node3, node4) \
 	do { \
-		log("%s: Unlocking nodes (%p, %p, %p, %p)...", __FUNCTION__, node1, node2, node3, node4); \
+		thread_t nodelock_thread = current_thread(); \
+		log("%s [%p]: Unlocking nodes (%p, %p, %p, %p)...", __FUNCTION__, nodelock_thread, node1, node2, node3, node4); \
 		fusefs_unlockfour(node1, node2, node3, node4); \
-		log("%s:   node pair (%p, %p, %p, %p) unlocked!", __FUNCTION__, node1, node2, node3, node4); \
+		log("%s [%p]:   node pair (%p, %p, %p, %p) unlocked!", __FUNCTION__, nodelock_thread, node1, node2, node3, node4); \
 	} while(0)
 
 /** Wrapper that surrounds a vfsop call with biglock locking. */

fuse_locking.h
@@ -21,6 +21,10 @@
 #  include <sys/vm.h>
 #endif
 
+#if M_OSXFUSE_ENABLE_INTERIM_FSNODE_LOCK
+#  include <kern/thread.h>
+#endif
+
 enum fusefslocktype {
     FUSEFS_SHARED_LOCK    = 1,
     FUSEFS_EXCLUSIVE_LOCK = 2,
@@ -145,16 +149,18 @@ extern fusefs_recursive_lock *fuse_huge_lock;
 
 #define fuse_hugelock_lock() \
 	do { \
-		log("%s: Aquiring huge lock %p...", __FUNCTION__, fuse_huge_lock); \
+		thread_t hugelock_thread = current_thread(); \
+		log("%s thread=%p: Aquiring huge lock %p...", __FUNCTION__, hugelock_thread, fuse_huge_lock); \
 		fusefs_recursive_lock_lock(fuse_huge_lock); \
-		log("%s:   huge lock %p aquired!", __FUNCTION__, fuse_huge_lock); \
+		log("%s thread=%p: huge lock %p aquired!", __FUNCTION__, hugelock_thread, fuse_huge_lock); \
 	} while(0)
 
 #define fuse_hugelock_unlock() \
 	do { \
-		log("%s: Releasing huge lock %p...", __FUNCTION__, fuse_huge_lock); \
+		thread_t hugelock_thread = current_thread(); \
+		log("%s thread=%p: Releasing huge lock %p...", __FUNCTION__, hugelock_thread, fuse_huge_lock); \
 		fusefs_recursive_lock_unlock(fuse_huge_lock); \
-		log("%s:   huge lock %p released!", __FUNCTION__, fuse_huge_lock); \
+		log("%s thread=%p: huge lock %p released!", __FUNCTION__, hugelock_thread, fuse_huge_lock); \
 	} while(0)
 
 #define fuse_hugelock_have_lock() fusefs_recursive_lock_have_lock(fuse_huge_lock)
@@ -172,16 +178,18 @@ typedef fusefs_recursive_lock fuse_biglock_t;
 
 #define fuse_biglock_lock(lock) \
 	do { \
-		log("%s: Aquiring biglock %p...", __FUNCTION__, lock); \
+		thread_t biglock_thread = current_thread(); \
+		log("%s thread=%p: Aquiring biglock %p...", __FUNCTION__, biglock_thread, lock); \
 		fusefs_recursive_lock_lock(lock); \
-		log("%s:   biglock %p aquired!", __FUNCTION__, lock); \
+		log("%s thread=%p: biglock %p aquired!", __FUNCTION__, biglock_thread, lock); \
 	} while(0)
 
 #define fuse_biglock_unlock(lock) \
 	do { \
-		log("%s: Releasing biglock %p...", __FUNCTION__, lock); \
+		thread_t biglock_thread = current_thread(); \
+		log("%s thread=%p: Releasing biglock %p...", __FUNCTION__, biglock_thread, lock); \
 		fusefs_recursive_lock_unlock(lock); \
-		log("%s:   biglock %p released!", __FUNCTION__, lock); \
+		log("%s thread=%p: biglock %p released!", __FUNCTION__, biglock_thread, lock); \
 	} while(0)
 
 #define fuse_biglock_have_lock(lock) fusefs_recursive_lock_have_lock(lock)

commit e22f128e9e5ad04e59adcadfd676c44243248c43

Fix compiler warning/error in case of non-debug build
Commit 0a079be6e9d191de985c9c0a194752f56cc48070 introduced compiler warnings/
errors for non-debug builds of the kernel extension:

warning: unused variable 'nodelock_thread'
warning: unused variable 'biglock_thread'

fuse_biglock_vnops.h
@@ -18,59 +18,53 @@
 
 #define fuse_nodelock_lock(node, type) \
 	do { \
-		thread_t nodelock_thread = current_thread(); \
 		int fnl_err; \
-		log("%s thread=%p: Locking node %p...", __FUNCTION__, nodelock_thread, node); \
+		log("%s thread=%p: Locking node %p...", __FUNCTION__, current_thread(), node); \
 		fnl_err = fusefs_lock(node, type); \
 		if(fnl_err) \
 			return fnl_err; \
-		log("%s thread=%p: node %p locked!", __FUNCTION__, nodelock_thread, node); \
+		log("%s thread=%p: node %p locked!", __FUNCTION__, current_thread(), node); \
 	} while(0)
 
 #define fuse_nodelock_unlock(node) \
 	do { \
-		thread_t nodelock_thread = current_thread(); \
-		log("%s thread=%p: Unlocking node %p...", __FUNCTION__, nodelock_thread, node); \
+		log("%s thread=%p: Unlocking node %p...", __FUNCTION__, current_thread(), node); \
 		fusefs_unlock(node); \
-		log("%s thread=%p: node %p unlocked!", __FUNCTION__, nodelock_thread, node); \
+		log("%s thread=%p: node %p unlocked!", __FUNCTION__, current_thread(), node); \
 	} while(0)
 
 #define fuse_nodelock_lock_pair(node1, node2, type) \
 	do { \
-		thread_t nodelock_thread = current_thread(); \
 		int fnl_err; \
-		log("%s thread=%p: Locking node pair (%p, %p)...", __FUNCTION__, nodelock_thread, node1, node2); \
+		log("%s thread=%p: Locking node pair (%p, %p)...", __FUNCTION__, current_thread(), node1, node2); \
 		fnl_err = fusefs_lockpair(node1, node2, type); \
 		if(fnl_err) \
 			return fnl_err; \
-		log("%s thread=%p: node pair (%p, %p) locked!", __FUNCTION__, nodelock_thread, node1, node2); \
+		log("%s thread=%p: node pair (%p, %p) locked!", __FUNCTION__, current_thread(), node1, node2); \
 	} while(0)
 
 #define fuse_nodelock_unlock_pair(node1, node2) \
 	do { \
-		thread_t nodelock_thread = current_thread(); \
-		log("%s thread=%p: Unlocking node pair (%p, %p)...", __FUNCTION__, nodelock_thread, node1, node2); \
+		log("%s thread=%p: Unlocking node pair (%p, %p)...", __FUNCTION__, current_thread(), node1, node2); \
 		fusefs_unlockpair(node1, node2); \
-		log("%s thread=%p: node pair (%p, %p) unlocked!", __FUNCTION__, nodelock_thread, node1, node2); \
+		log("%s thread=%p: node pair (%p, %p) unlocked!", __FUNCTION__, current_thread(), node1, node2); \
 	} while(0)
 
 #define fuse_nodelock_lock_four(node1, node2, node3, node4, type) \
 	do { \
-		thread_t nodelock_thread = current_thread(); \
 		int fnl_err; \
-		log("%s thread=%p: Locking node pair (%p, %p, %p, %p)...", __FUNCTION__, nodelock_thread, node1, node2, node3, node4); \
+		log("%s thread=%p: Locking node pair (%p, %p, %p, %p)...", __FUNCTION__, current_thread(), node1, node2, node3, node4); \
 		fnl_err = fusefs_lockfour(node1, node2, node3, node4, type); \
 		if(fnl_err) \
 			return fnl_err; \
-		log("%s thread=%p: node pair (%p, %p, %p, %p) locked!", __FUNCTION__, nodelock_thread, node1, node2, node3, node4); \
+		log("%s thread=%p: node pair (%p, %p, %p, %p) locked!", __FUNCTION__, current_thread(), node1, node2, node3, node4); \
 	} while(0)
 
 #define fuse_nodelock_unlock_four(node1, node2, node3, node4) \
 	do { \
-		thread_t nodelock_thread = current_thread(); \
-		log("%s [%p]: Unlocking nodes (%p, %p, %p, %p)...", __FUNCTION__, nodelock_thread, node1, node2, node3, node4); \
+		log("%s [%p]: Unlocking nodes (%p, %p, %p, %p)...", __FUNCTION__, current_thread(), node1, node2, node3, node4); \
 		fusefs_unlockfour(node1, node2, node3, node4); \
-		log("%s [%p]:   node pair (%p, %p, %p, %p) unlocked!", __FUNCTION__, nodelock_thread, node1, node2, node3, node4); \
+		log("%s [%p]:   node pair (%p, %p, %p, %p) unlocked!", __FUNCTION__, current_thread(), node1, node2, node3, node4); \
 	} while(0)
 
 /** Wrapper that surrounds a vfsop call with biglock locking. */

fuse_locking.h
@@ -149,18 +149,16 @@ extern fusefs_recursive_lock *fuse_huge_lock;
 
 #define fuse_hugelock_lock() \
 	do { \
-		thread_t hugelock_thread = current_thread(); \
-		log("%s thread=%p: Aquiring huge lock %p...", __FUNCTION__, hugelock_thread, fuse_huge_lock); \
+		log("%s thread=%p: Aquiring huge lock %p...", __FUNCTION__, current_thread(), fuse_huge_lock); \
 		fusefs_recursive_lock_lock(fuse_huge_lock); \
-		log("%s thread=%p: huge lock %p aquired!", __FUNCTION__, hugelock_thread, fuse_huge_lock); \
+		log("%s thread=%p: huge lock %p aquired!", __FUNCTION__, current_thread(), fuse_huge_lock); \
 	} while(0)
 
 #define fuse_hugelock_unlock() \
 	do { \
-		thread_t hugelock_thread = current_thread(); \
-		log("%s thread=%p: Releasing huge lock %p...", __FUNCTION__, hugelock_thread, fuse_huge_lock); \
+		log("%s thread=%p: Releasing huge lock %p...", __FUNCTION__, current_thread(), fuse_huge_lock); \
 		fusefs_recursive_lock_unlock(fuse_huge_lock); \
-		log("%s thread=%p: huge lock %p released!", __FUNCTION__, hugelock_thread, fuse_huge_lock); \
+		log("%s thread=%p: huge lock %p released!", __FUNCTION__, current_thread(), fuse_huge_lock); \
 	} while(0)
 
 #define fuse_hugelock_have_lock() fusefs_recursive_lock_have_lock(fuse_huge_lock)
@@ -178,18 +176,16 @@ typedef fusefs_recursive_lock fuse_biglock_t;
 
 #define fuse_biglock_lock(lock) \
 	do { \
-		thread_t biglock_thread = current_thread(); \
-		log("%s thread=%p: Aquiring biglock %p...", __FUNCTION__, biglock_thread, lock); \
+		log("%s thread=%p: Aquiring biglock %p...", __FUNCTION__, current_thread(), lock); \
 		fusefs_recursive_lock_lock(lock); \
-		log("%s thread=%p: biglock %p aquired!", __FUNCTION__, biglock_thread, lock); \
+		log("%s thread=%p: biglock %p aquired!", __FUNCTION__, current_thread(), lock); \
 	} while(0)
 
 #define fuse_biglock_unlock(lock) \
 	do { \
-		thread_t biglock_thread = current_thread(); \
-		log("%s thread=%p: Releasing biglock %p...", __FUNCTION__, biglock_thread, lock); \
+		log("%s thread=%p: Releasing biglock %p...", __FUNCTION__, current_thread(), lock); \
 		fusefs_recursive_lock_unlock(lock); \
-		log("%s thread=%p: biglock %p released!", __FUNCTION__, biglock_thread, lock); \
+		log("%s thread=%p: biglock %p released!", __FUNCTION__, current_thread(), lock); \
 	} while(0)
 
 #define fuse_biglock_have_lock(lock) fusefs_recursive_lock_have_lock(lock)