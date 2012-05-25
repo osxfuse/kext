osxfuse/kext

commit a47aa995dad4d65521b0b5bad826b98fbbc76f44

Implement ticket reference counting
Instead of explicitly dropping a ticket decrement its reference count. If the
reference count reaches 0 the ticket is dropped automatically.

fuse_device.c
@@ -264,6 +264,7 @@ fuse_device_close(dev_t dev, __unused int flags, __unused int devtype,
             ftick->tk_aw_errno = ENOTCONN;
             fuse_wakeup(ftick);
             fuse_lck_mtx_unlock(ftick->tk_aw_mtx);
+            fuse_ticket_release(ftick);
         }
 
         fuse_lck_mtx_unlock(data->aw_mtx);
@@ -348,7 +349,7 @@ again:
 
     if (fdata_dead_get(data)) {
          if (ftick) {
-             fuse_ticket_drop_invalid(ftick);
+             fuse_ticket_release(ftick);
          }
          return ENODEV;
     }
@@ -392,14 +393,7 @@ again:
         err = EINTR;
     }
 
-    /*
-     * The FORGET message is an example of a ticket that has explicitly
-     * been invalidated by the sender. The sender is not expecting or wanting
-     * a reply, so he sets the FT_INVALID bit in the ticket.
-     */
-
-    fuse_ticket_drop_invalid(ftick);
-
+    fuse_ticket_release(ftick);
     return err;
 }
 
@@ -465,10 +459,8 @@ fuse_device_write(dev_t dev, uio_t uio, __unused int ioflag)
         if (ftick->tk_aw_handler) {
             memcpy(&ftick->tk_aw_ohead, &ohead, sizeof(ohead));
             err = ftick->tk_aw_handler(ftick, uio);
-        } else {
-            fuse_ticket_drop(ftick);
-            return err;
         }
+        fuse_ticket_release(ftick);
     } else {
         /* ticket has no response handler */
     }
@@ -775,6 +767,7 @@ fuse_device_kill(int unit, struct proc *p)
                         ftick->tk_aw_errno = ENOTCONN;
                         fuse_wakeup(ftick);
                         fuse_lck_mtx_unlock(ftick->tk_aw_mtx);
+                        fuse_ticket_release(ftick);
                     }
                 }
                 fuse_lck_mtx_unlock(fdev->data->aw_mtx);

fuse_file.c
@@ -112,7 +112,7 @@ fuse_filehandle_get(vnode_t       vp,
     fufh->fuse_open_flags = foo->open_flags;
     fufh->aux_count = 0;
 
-    fuse_ticket_drop(fdi.tick);
+    fuse_ticket_release(fdi.tick);
 
     return 0;
 }
@@ -160,11 +160,12 @@ fuse_filehandle_put(vnode_t vp, vfs_context_t context, fufh_type_t fufh_type,
         if ((err = fdisp_wait_answ(&fdi))) {
             goto out;
         } else {
-            fuse_ticket_drop(fdi.tick);
+            fuse_ticket_release(fdi.tick);
         }
     } else {
         fuse_insert_callback(fdi.tick, NULL);
         fuse_insert_message(fdi.tick);
+        fuse_ticket_release(fdi.tick);
     }
 
 out:

fuse_internal.c
@@ -147,7 +147,7 @@ fuse_internal_access(vnode_t                   vp,
     fai->mask |= mask;
 
     if (!(err = fdisp_wait_answ(&fdi))) {
-        fuse_ticket_drop(fdi.tick);
+        fuse_ticket_release(fdi.tick);
     }
 
     if (err == ENOSYS) {
@@ -236,7 +236,7 @@ fuse_internal_exchange(vnode_t       fvp,
               UBC_PUSHALL | UBC_INVALIDATE | UBC_SYNC);
 
     if (!(err = fdisp_wait_answ(&fdi))) {
-        fuse_ticket_drop(fdi.tick);
+        fuse_ticket_release(fdi.tick);
     }
 
     if (err == 0) {
@@ -312,7 +312,7 @@ fuse_internal_fsync_callback(struct fuse_ticket *ftick, __unused uio_t uio)
         }
     }
 
-    fuse_ticket_drop(ftick);
+    fuse_ticket_release(ftick);
 
     return 0;
 }
@@ -357,11 +357,12 @@ fuse_internal_fsync(vnode_t                 vp,
             }
             goto out;
         } else {
-            fuse_ticket_drop(fdip->tick);
+            fuse_ticket_release(fdip->tick);
         }
     } else {
         fuse_insert_callback(fdip->tick, fuse_internal_fsync_callback);
         fuse_insert_message(fdip->tick);
+        fuse_ticket_release(fdip->tick);
     }
 
 out:
@@ -425,7 +426,7 @@ fuse_internal_loadxtimes(vnode_t vp, struct vnode_attr *out_vap,
     VATTR_RETURN(in_vap, va_create_time, t);
     VATTR_RETURN(out_vap, va_create_time, t);
 
-    fuse_ticket_drop(fdi.tick);
+    fuse_ticket_release(fdi.tick);
 
     VTOFUD(vp)->c_flag |= C_XTIMES_VALID;
 
@@ -715,7 +716,7 @@ fuse_internal_readdir(vnode_t                 vp,
 
 /* done: */
 
-    fuse_ticket_drop(fdi.tick);
+    fuse_ticket_release(fdi.tick);
 
 out:
     return ((err == -1) ? 0 : err);
@@ -884,7 +885,7 @@ fuse_internal_remove(vnode_t               dvp,
     }
 
     if (!(err = fdisp_wait_answ(&fdi))) {
-        fuse_ticket_drop(fdi.tick);
+        fuse_ticket_release(fdi.tick);
     }
 
     fuse_invalidate_attr(dvp);
@@ -950,7 +951,7 @@ fuse_internal_rename(vnode_t               fdvp,
                          tcnp->cn_namelen + 1] = '\0';
 
     if (!(err = fdisp_wait_answ(&fdi))) {
-        fuse_ticket_drop(fdi.tick);
+        fuse_ticket_release(fdi.tick);
     }
 
     if (err == 0) {
@@ -1285,7 +1286,7 @@ fuse_internal_strategy(vnode_t vp, buf_t bp)
     }
 
     if (fdi.tick) {
-        fuse_ticket_drop(fdi.tick);
+        fuse_ticket_release(fdi.tick);
     } else {
         /* No ticket upon leaving */
     }
@@ -1436,7 +1437,7 @@ fuse_internal_newentry_core(vnode_t                 dvp,
     cache_attrs(*vpp, feo);
 
 out:
-    fuse_ticket_drop(fdip->tick);
+    fuse_ticket_release(fdip->tick);
 
     return err;
 }
@@ -1508,6 +1509,7 @@ fuse_internal_forget_send(mount_t                 mp,
 
     fticket_invalidate(fdip->tick);
     fuse_insert_message(fdip->tick);
+    fuse_ticket_release(fdip->tick);
 }
 
 __private_extern__
@@ -1525,6 +1527,7 @@ fuse_internal_interrupt_send(struct fuse_ticket *ftick)
     fii->unique = ftick->tk_unique;
     fticket_invalidate(fdi.tick);
     fuse_insert_message(fdi.tick);
+    fuse_ticket_release(fdi.tick);
 }
 
 __private_extern__
@@ -1618,7 +1621,7 @@ fuse_internal_init_synchronous(struct fuse_ticket *ftick)
     }
 
 out:
-    fuse_ticket_drop(ftick);
+    fuse_ticket_release(ftick);
 
     if (err) {
         fdata_set_dead(data);
@@ -1661,6 +1664,8 @@ fuse_internal_send_init(struct fuse_data *data, vfs_context_t context)
         IOLog("OSXFUSE: in-kernel initialization failed (%d)\n", err);
         return err;
     }
+
+    /* fuse_ticket_release is called in fuse_internal_init_synchronous */
 
     return 0;
 }

fuse_ipc.c
@@ -598,6 +598,7 @@ fuse_ticket_fetch(struct fuse_data *data)
             panic("OSXFUSE: no free ticket despite the counter's value");
         }
     }
+    ftick->tk_ref_count = 1;
 
     if (!(data->dataflags & FSESS_INITED) && data->ticketer > 1) {
         err = fuse_msleep(&data->ticketer, data->ticket_mtx, PCATCH | PDROP,
@@ -614,6 +615,7 @@ fuse_ticket_fetch(struct fuse_data *data)
         fdata_set_dead(data);
     }
 
+    ftick->tk_ref_count = 1;
     return ftick;
 }
 
@@ -658,7 +660,7 @@ void
 fuse_ticket_drop_invalid(struct fuse_ticket *ftick)
 {
     if (ftick->tk_flag & FT_INVAL) {
-        fuse_ticket_drop(ftick);
+        fuse_ticket_release(ftick);
     }
 }
 
@@ -669,6 +671,7 @@ fuse_insert_callback(struct fuse_ticket *ftick, fuse_handler_t *handler)
         return;
     }
 
+    fuse_ticket_retain(ftick);
     ftick->tk_aw_handler = handler;
 
     fuse_lck_mtx_lock(ftick->tk_data->aw_mtx);
@@ -683,11 +686,12 @@ fuse_insert_message(struct fuse_ticket *ftick)
         panic("OSXFUSE: ticket reused without being refreshed");
     }
 
-    ftick->tk_flag |= FT_DIRTY;
-
     if (fdata_dead_get(ftick->tk_data)) {
         return;
     }
+
+    fuse_ticket_retain(ftick);
+    ftick->tk_flag |= FT_DIRTY;
 
     fuse_lck_mtx_lock(ftick->tk_data->ms_mtx);
     fuse_ms_push(ftick);
@@ -710,6 +714,8 @@ fuse_insert_message_head(struct fuse_ticket *ftick)
     if (fdata_dead_get(ftick->tk_data)) {
         return;
     }
+
+    fuse_ticket_retain(ftick);
 
     fuse_lck_mtx_lock(ftick->tk_data->ms_mtx);
     fuse_ms_push_head(ftick);
@@ -958,7 +964,7 @@ fuse_standard_handler(struct fuse_ticket *ftick, uio_t uio)
     fuse_lck_mtx_unlock(ftick->tk_aw_mtx);
 
     if (dropflag) {
-        fuse_ticket_drop(ftick);
+        fuse_ticket_release(ftick);
     }
 
     return err;
@@ -974,6 +980,9 @@ fdisp_make(struct fuse_dispatcher *fdip,
     struct fuse_data *data = fuse_get_mpdata(mp);
 
     if (fdip->tick) {
+        if (fdip->tick->tk_ref_count != 1) {
+            panic("OSXFUSE: ticket cannot be reused");
+        }
         fticket_refresh(fdip->tick);
     } else {
         fdip->tick = fuse_ticket_fetch(data);
@@ -1002,6 +1011,9 @@ fdisp_make_canfail(struct fuse_dispatcher *fdip,
     struct fuse_data *data = fuse_get_mpdata(mp);
 
     if (fdip->tick) {
+        if (fdip->tick->tk_ref_count != 1) {
+            panic("OSXFUSE: ticket cannot be reused");
+        }
         fticket_refresh(fdip->tick);
     } else {
         fdip->tick = fuse_ticket_fetch(data);
@@ -1116,7 +1128,7 @@ fdisp_wait_answ(struct fuse_dispatcher *fdip)
     return 0;
 
 out:
-    fuse_ticket_drop(fdip->tick);
+    fuse_ticket_release(fdip->tick);
 
     return err;
 }

fuse_ipc.h
@@ -63,6 +63,7 @@ struct fuse_ticket {
     struct fuse_data            *tk_data;
     int                          tk_flag;
     uint32_t                     tk_age;
+    uint32_t                     tk_ref_count;
 
     STAILQ_ENTRY(fuse_ticket)    tk_freetickets_link;
     TAILQ_ENTRY(fuse_ticket)     tk_alltickets_link;
@@ -301,6 +302,50 @@ struct fuse_ticket *fuse_ticket_fetch(struct fuse_data *data);
 void fuse_ticket_drop(struct fuse_ticket *ftick);
 void fuse_ticket_drop_invalid(struct fuse_ticket *ftick);
 void fuse_ticket_kill(struct fuse_ticket *ftick);
+
+#ifndef OSCompareAndSwap
+#  define OSCompareAndSwap(a, b, c) OSCompareAndSwap(a, b, (volatile UInt32*)c)
+#endif
+
+/*
+ * Increases the reference count of the specified ticket.
+ */
+static __inline__
+void
+fuse_ticket_retain(struct fuse_ticket *ticket)
+{
+    int count;
+
+    do {
+        count = ticket->tk_ref_count;
+    } while (!OSCompareAndSwap(count, count + 1, &(ticket->tk_ref_count)));
+
+    if (count == 0) {
+        panic("OSXFUSE: fuse_ticket_retain: ticket reference count is 0");
+    }
+}
+
+/*
+ * Decrements the reference count of the specified ticket. If the count reaches
+ * 0 the ticket is dropped instantly.
+ */
+static __inline__
+void
+fuse_ticket_release(struct fuse_ticket *ticket) {
+    int count;
+
+    do {
+        count = ticket->tk_ref_count;
+    } while (!OSCompareAndSwap(count, count - 1, &(ticket->tk_ref_count)));
+
+    if (count == 0) {
+        panic("OSXFUSE: fuse_ticket_release: ticket reference count is 0");
+    }
+    if (count == 1) {
+        fuse_ticket_drop(ticket);
+    }
+}
+
 void fuse_insert_callback(struct fuse_ticket *ftick, fuse_handler_t *handler);
 void fuse_insert_message(struct fuse_ticket *ftick);
 void fuse_insert_message_head(struct fuse_ticket *ftick);

fuse_vfsops.c
@@ -706,7 +706,7 @@ fuse_vfsop_unmount(mount_t mp, int mntflags, vfs_context_t context)
 
     err = fdisp_wait_answ(&fdi);
     if (!err) {
-        fuse_ticket_drop(fdi.tick);
+        fuse_ticket_release(fdi.tick);
     }
 
     /*
@@ -1211,7 +1211,7 @@ dostatfs:
     VFSATTR_RETURN(attr, f_carbon_fsid, 0);
 
     if (faking == 0) {
-        fuse_ticket_drop(fdi.tick);
+        fuse_ticket_release(fdi.tick);
     }
 
     return 0;
@@ -1378,7 +1378,7 @@ fuse_vfsop_setattr(mount_t mp, struct vfs_attr *fsap, vfs_context_t context)
         ((char *)fdi.indata)[namelen] = '\0';
 
         if (!(error = fdisp_wait_answ(&fdi))) {
-            fuse_ticket_drop(fdi.tick);
+            fuse_ticket_release(fdi.tick);
         }
 
         (void)vnode_put(root_vp);

commit 9f348b9775ca93ec1278ef1b0333259a2116559e

Finish ticket reference counting
* Fix ticket leak introduced in commit a47aa995dad4d65521b0b5bad826b98fbbc76f44

* Work around a race condition that can trigger a "ticket cannot be reused"
  kernel panic when tickets are being reused without being dropped first
  (fuse_vnop_strategy, fuse_vnop_read, fuse_vnop_write, ...).

fuse_device.c
@@ -351,6 +351,18 @@ again_locked:
 
     fuse_lck_mtx_lock(ftick->tk_aw_mtx);
     if (fticket_answered(ftick)) {
+        fuse_remove_callback(ftick);
+
+        if (ftick->tk_interrupt) {
+            struct fuse_ticket *interrupt = ftick->tk_interrupt;
+
+            fuse_internal_interrupt_remove(interrupt);
+
+            /* Release interrupt ticket retained in fuse_internal_interrupt_send */
+            ftick->tk_interrupt = NULL;
+            fuse_ticket_release(interrupt);
+        }
+
         fuse_lck_mtx_unlock(ftick->tk_aw_mtx);
         fuse_ticket_release(ftick);
 

fuse_file.c
@@ -159,14 +159,12 @@ fuse_filehandle_put(vnode_t vp, vfs_context_t context, fufh_type_t fufh_type,
     if (waitfor == FUSE_OP_FOREGROUNDED) {
         if ((err = fdisp_wait_answ(&fdi))) {
             goto out;
-        } else {
-            fuse_ticket_release(fdi.tick);
         }
     } else {
         fuse_insert_callback(fdi.tick, NULL);
         fuse_insert_message(fdi.tick);
-        fuse_ticket_release(fdi.tick);
     }
+    fuse_ticket_release(fdi.tick);
 
 out:
     FUSE_OSAddAtomic(-1, (SInt32 *)&fuse_fh_current);

fuse_internal.c
@@ -312,8 +312,6 @@ fuse_internal_fsync_callback(struct fuse_ticket *ftick, __unused uio_t uio)
         }
     }
 
-    fuse_ticket_release(ftick);
-
     return 0;
 }
 
@@ -322,48 +320,44 @@ int
 fuse_internal_fsync(vnode_t                 vp,
                     vfs_context_t           context,
                     struct fuse_filehandle *fufh,
-                    void                   *param,
                     fuse_op_waitfor_t       waitfor)
 {
     int err = 0;
     int op = FUSE_FSYNC;
     struct fuse_fsync_in *ffsi;
-    struct fuse_dispatcher *fdip = param;
+    struct fuse_dispatcher fdi;
 
     fuse_trace_printf_func();
 
-    fdip->iosize = sizeof(*ffsi);
-    fdip->tick = NULL;
+    fdisp_init(&fdi, sizeof(*ffsi));
     if (vnode_isdir(vp)) {
         op = FUSE_FSYNCDIR;
     }
 
-    fdisp_make_vp(fdip, op, vp, context);
-    ffsi = fdip->indata;
+    fdisp_make_vp(&fdi, op, vp, context);
+    ffsi = fdi.indata;
     ffsi->fh = fufh->fh_id;
 
     ffsi->fsync_flags = 1; /* datasync */
 
     if (waitfor == FUSE_OP_FOREGROUNDED) {
-        if ((err = fdisp_wait_answ(fdip))) {
+        if ((err = fdisp_wait_answ(&fdi))) {
             if (err == ENOSYS) {
                 if (op == FUSE_FSYNC) {
-                    fuse_clear_implemented(fdip->tick->tk_data,
+                    fuse_clear_implemented(fdi.tick->tk_data,
                                            FSESS_NOIMPLBIT(FSYNC));
                 } else if (op == FUSE_FSYNCDIR) {
-                    fuse_clear_implemented(fdip->tick->tk_data,
+                    fuse_clear_implemented(fdi.tick->tk_data,
                                            FSESS_NOIMPLBIT(FSYNCDIR));
                 }
             }
             goto out;
-        } else {
-            fuse_ticket_release(fdip->tick);
         }
     } else {
-        fuse_insert_callback(fdip->tick, fuse_internal_fsync_callback);
-        fuse_insert_message(fdip->tick);
-        fuse_ticket_release(fdip->tick);
+        fuse_insert_callback(fdi.tick, fuse_internal_fsync_callback);
+        fuse_insert_message(fdi.tick);
     }
+    fuse_ticket_release(fdi.tick);
 
 out:
     return err;
@@ -1401,7 +1395,7 @@ fuse_internal_newentry_makerequest(mount_t                 mp,
                                    struct fuse_dispatcher *fdip,
                                    vfs_context_t           context)
 {
-    fdisp_init(fdip, bufsize + cnp->cn_namelen + 1);
+    fdip->iosize = bufsize + cnp->cn_namelen + 1;
 
     fdisp_make(fdip, op, mp, dnid, context);
     memcpy(fdip->indata, buf, bufsize);
@@ -1435,7 +1429,7 @@ fuse_internal_newentry_core(vnode_t                 dvp,
     err = fuse_vget_i(vpp, 0 /* flags */, feo, cnp, dvp, mp, context);
     if (err) {
         fuse_internal_forget_send(mp, context, feo->nodeid, 1, fdip);
-        return err;
+        goto out;
     }
 
     cache_attrs(*vpp, feo);
@@ -1468,6 +1462,7 @@ fuse_internal_newentry(vnode_t               dvp,
     fdisp_init(&fdi, 0);
     fuse_internal_newentry_makerequest(mp, VTOI(dvp), cnp, op, buf,
                                        bufsize, &fdi, context);
+    /* Note: fuse_internal_newentry_core releases fdi.tick */
     err = fuse_internal_newentry_core(dvp, vpp, cnp, vtype, &fdi, context);
     fuse_invalidate_attr(dvp);
 
@@ -1487,6 +1482,7 @@ fuse_internal_forget_callback(struct fuse_ticket *ftick, __unused uio_t uio)
     fuse_internal_forget_send(ftick->tk_data->mp, (vfs_context_t)0,
         ((struct fuse_in_header *)ftick->tk_ms_fiov.base)->nodeid, 1, &fdi);
 
+    fuse_ticket_release(fdi.tick);
     return 0;
 }
 
@@ -1505,15 +1501,13 @@ fuse_internal_forget_send(mount_t                 mp,
      *         (long long unsigned) nodeid));
      */
 
-    fdisp_init(fdip, sizeof(*ffi));
+    fdip->iosize = sizeof(*ffi);
     fdisp_make(fdip, FUSE_FORGET, mp, nodeid, context);
 
     ffi = fdip->indata;
     ffi->nlookup = nlookup;
 
-    fticket_invalidate(fdip->tick);
     fuse_insert_message(fdip->tick);
-    fuse_ticket_release(fdip->tick);
 }
 
 static int
@@ -1574,6 +1568,23 @@ fuse_internal_interrupt_send(struct fuse_ticket *ftick)
      * Note: The interrupt ticket is released in fuse_standard_handler when
      * processing the answer to the original ticket.
      */
+}
+
+__private_extern__
+void
+fuse_internal_interrupt_remove(struct fuse_ticket *interrupt)
+{
+    fuse_lck_mtx_lock(interrupt->tk_aw_mtx);
+
+    /*
+     * Note: Pending requests that are already marked as answered will not
+     * not be sent to user space. See fuse_device_read().
+     */
+    fticket_set_answered(interrupt);
+
+    fuse_remove_callback(interrupt);
+
+    fuse_lck_mtx_unlock(interrupt->tk_aw_mtx);
 }
 
 __private_extern__

fuse_internal.h
@@ -712,7 +712,6 @@ int
 fuse_internal_fsync(vnode_t                 vp,
                     vfs_context_t           context,
                     struct fuse_filehandle *fufh,
-                    void                   *param,
                     fuse_op_waitfor_t       waitfor);
 
 int
@@ -867,6 +866,9 @@ fuse_internal_forget_send(mount_t                 mp,
 
 void
 fuse_internal_interrupt_send(struct fuse_ticket *ftick);
+
+void
+fuse_internal_interrupt_remove(struct fuse_ticket *interrupt);
 
 enum {
     REVOKE_NONE = 0,

fuse_ipc.c
@@ -641,7 +641,6 @@ fuse_ticket_fetch(struct fuse_data *data)
         fdata_set_dead(data);
     }
 
-    ftick->tk_ref_count = 1;
     return ftick;
 }
 
@@ -649,7 +648,7 @@ void
 fuse_ticket_drop(struct fuse_ticket *ftick)
 {
     int die = 0;
-    
+
     fuse_lck_mtx_lock(ftick->tk_data->ticket_mtx);
 
     if (fuse_max_freetickets <= ftick->tk_data->freeticket_counter ||
@@ -706,6 +705,23 @@ fuse_insert_callback(struct fuse_ticket *ftick, fuse_handler_t *handler)
 }
 
 void
+fuse_remove_callback(struct fuse_ticket *ftick)
+{
+    struct fuse_data *data = ftick->tk_data;
+    struct fuse_ticket *curr;
+
+    fuse_lck_mtx_lock(data->aw_mtx);
+    TAILQ_FOREACH(curr, &data->aw_head, tk_aw_link) {
+        if (curr == ftick) {
+            fuse_aw_remove(curr);
+            fuse_ticket_release(curr);
+            break;
+        }
+    }
+    fuse_lck_mtx_unlock(data->aw_mtx);
+}
+
+void
 fuse_insert_message(struct fuse_ticket *ftick)
 {
     if (ftick->tk_flag & FT_DIRTY) {
@@ -735,13 +751,12 @@ fuse_insert_message_head(struct fuse_ticket *ftick)
         panic("OSXFUSE: ticket reused without being refreshed");
     }
 
-    ftick->tk_flag |= FT_DIRTY;
-
     if (fdata_dead_get(ftick->tk_data)) {
         return;
     }
 
     fuse_ticket_retain(ftick);
+    ftick->tk_flag |= FT_DIRTY;
 
     fuse_lck_mtx_lock(ftick->tk_data->ms_mtx);
     fuse_ms_push_head(ftick);
@@ -979,33 +994,12 @@ fuse_standard_handler(struct fuse_ticket *ftick, uio_t uio)
     fuse_lck_mtx_lock(ftick->tk_aw_mtx);
 
     if (ftick->tk_interrupt) {
-        struct fuse_data *data = ftick->tk_data;
         struct fuse_ticket *interrupt = ftick->tk_interrupt;
-        struct fuse_ticket *curr;
 
-        fuse_lck_mtx_lock(interrupt->tk_aw_mtx);
-
-        /*
-         * Note: Pending requests that are already marked as answered will not
-         * not be sent to user space. See fuse_device_read().
-         */
-        fticket_set_answered(interrupt);
-
-        fuse_lck_mtx_lock(data->aw_mtx);
-        TAILQ_FOREACH(curr, &data->aw_head, tk_aw_link) {
-            if (curr == interrupt) {
-                fuse_aw_remove(curr);
-                fuse_ticket_release(curr);
-                break;
-            }
-        }
-        fuse_lck_mtx_unlock(data->aw_mtx);
-
-        ftick->tk_interrupt = NULL;
-
-        fuse_lck_mtx_unlock(interrupt->tk_aw_mtx);
+        fuse_internal_interrupt_remove(interrupt);
 
         /* Release interrupt ticket retained in fuse_internal_interrupt_send */
+        ftick->tk_interrupt = NULL;
         fuse_ticket_release(interrupt);
     }
 
@@ -1030,9 +1024,6 @@ fdisp_make(struct fuse_dispatcher *fdip,
     struct fuse_data *data = fuse_get_mpdata(mp);
 
     if (fdip->tick) {
-        if (fdip->tick->tk_ref_count != 1) {
-            panic("OSXFUSE: ticket cannot be reused");
-        }
         fticket_refresh(fdip->tick);
     } else {
         fdip->tick = fuse_ticket_fetch(data);
@@ -1041,6 +1032,31 @@ fdisp_make(struct fuse_dispatcher *fdip,
     if (fdip->tick == 0) {
         panic("OSXFUSE: fuse_ticket_fetch() failed");
     }
+
+#ifdef FUSE_TRACE_TICKET
+    if (fdip->tick->tk_age == 1) {
+        int aw_count = 0;
+        int ms_count = 0;
+
+        struct fuse_ticket    *ftick;
+        struct fuse_ticket    *x_ftick;
+
+        fuse_lck_mtx_lock(data->ms_mtx);
+        STAILQ_FOREACH_SAFE(ftick, &data->ms_head, tk_ms_link, x_ftick) {
+            ms_count++;
+        }
+        fuse_lck_mtx_unlock(data->ms_mtx);
+
+        fuse_lck_mtx_lock(data->aw_mtx);
+        TAILQ_FOREACH_SAFE(ftick, &data->aw_head, tk_aw_link, x_ftick) {
+            aw_count++;
+        }
+        fuse_lck_mtx_unlock(data->aw_mtx);
+
+        IOLog("OSXFUSE: new ticket created op=%d ms_count=%d aw_count=%d\n",
+              op, ms_count, aw_count);
+    }
+#endif
 
     FUSE_DIMALLOC(&fdip->tick->tk_ms_fiov, fdip->finh,
                   fdip->indata, fdip->iosize);
@@ -1061,9 +1077,6 @@ fdisp_make_canfail(struct fuse_dispatcher *fdip,
     struct fuse_data *data = fuse_get_mpdata(mp);
 
     if (fdip->tick) {
-        if (fdip->tick->tk_ref_count != 1) {
-            panic("OSXFUSE: ticket cannot be reused");
-        }
         fticket_refresh(fdip->tick);
     } else {
         fdip->tick = fuse_ticket_fetch(data);
@@ -1130,12 +1143,12 @@ fdisp_wait_answ(struct fuse_dispatcher *fdip)
         if (fticket_answered(fdip->tick)) {
             /* IPC: already answered */
             fuse_lck_mtx_unlock(fdip->tick->tk_aw_mtx);
-            goto out;
         } else {
             /* IPC: explicitly setting to answered */
             age = fdip->tick->tk_age;
             fticket_set_answered(fdip->tick);
             fuse_lck_mtx_unlock(fdip->tick->tk_aw_mtx);
+
 #ifndef DONT_TRY_HARD_PREVENT_IO_IN_VAIN
             fuse_lck_mtx_lock(fdip->tick->tk_data->aw_mtx);
             TAILQ_FOREACH(ftick, &fdip->tick->tk_data->aw_head, tk_aw_link) {
@@ -1147,11 +1160,11 @@ fdisp_wait_answ(struct fuse_dispatcher *fdip)
                     break;
                 }
             }
-
             fuse_lck_mtx_unlock(fdip->tick->tk_data->aw_mtx);
 #endif
-            return err;
         }
+
+        goto out;
     }
 
     /* IPC was NOT interrupt */

fuse_ipc.h
@@ -127,13 +127,6 @@ fticket_opcode(struct fuse_ticket *ftick)
     return (((struct fuse_in_header *)(ftick->tk_ms_fiov.base))->opcode);
 }
 
-static __inline__
-void
-fticket_invalidate(struct fuse_ticket *ftick)
-{
-    ftick->tk_flag |= FT_INVAL;
-}
-
 int fticket_pull(struct fuse_ticket *ftick, uio_t uio);
 
 enum mount_state { FM_NOTMOUNTED, FM_MOUNTED };
@@ -353,6 +346,7 @@ fuse_ticket_release(struct fuse_ticket *ticket) {
 }
 
 void fuse_insert_callback(struct fuse_ticket *ftick, fuse_handler_t *handler);
+void fuse_remove_callback(struct fuse_ticket *ftick);
 void fuse_insert_message(struct fuse_ticket *ftick);
 void fuse_insert_message_head(struct fuse_ticket *ftick);
 

fuse_vfsops.c
@@ -1229,7 +1229,6 @@ fuse_sync_callback(vnode_t vp, void *cargs)
     int type;
     struct fuse_sync_cargs *args;
     struct fuse_vnode_data *fvdat;
-    struct fuse_dispatcher  fdi;
     struct fuse_filehandle *fufh;
     struct fuse_data       *data;
     mount_t mp;
@@ -1256,11 +1255,10 @@ fuse_sync_callback(vnode_t vp, void *cargs)
 
     cluster_push(vp, 0);
 
-    fdisp_init(&fdi, 0);
     for (type = 0; type < FUFH_MAXTYPE; type++) {
         fufh = &(fvdat->fufh[type]);
         if (FUFH_IS_VALID(fufh)) {
-            (void)fuse_internal_fsync(vp, args->context, fufh, &fdi,
+            (void)fuse_internal_fsync(vp, args->context, fufh,
                                       FUSE_OP_FOREGROUNDED);
         }
     }

fuse_vnops.c
@@ -411,23 +411,23 @@ bringup:
               vpp, (gone_good_old) ? 0 : FN_CREATING,
               feo, mp, dvp, context, NULL /* oflags */);
     if (err) {
-       if (gone_good_old) {
-           fuse_internal_forget_send(mp, context, feo->nodeid, 1, fdip);
-       } else {
-           struct fuse_release_in *fri;
-           uint64_t nodeid = feo->nodeid;
-           uint64_t fh_id = ((struct fuse_open_out *)(feo + 1))->fh;
+        if (gone_good_old) {
+            fuse_internal_forget_send(mp, context, feo->nodeid, 1, fdip);
+        } else {
+            struct fuse_release_in *fri;
+            uint64_t nodeid = feo->nodeid;
+            uint64_t fh_id = ((struct fuse_open_out *)(feo + 1))->fh;
 
-           fdisp_init(fdip, sizeof(*fri));
-           fdisp_make(fdip, FUSE_RELEASE, mp, nodeid, context);
-           fri = fdip->indata;
-           fri->fh = fh_id;
-           fri->flags = OFLAGS(mode);
-           fuse_insert_callback(fdip->tick, fuse_internal_forget_callback);
-           fuse_insert_message(fdip->tick);
-           fuse_ticket_release(fdip->tick);
-       }
-       return err;
+            fdi.iosize = sizeof(*fri);
+            fdisp_make(fdip, FUSE_RELEASE, mp, nodeid, context);
+            fri = fdip->indata;
+            fri->fh = fh_id;
+            fri->flags = OFLAGS(mode);
+            fuse_insert_callback(fdip->tick, fuse_internal_forget_callback);
+            fuse_insert_message(fdip->tick);
+        }
+        fuse_ticket_release(fdip->tick);
+        return err;
     }
 
     fdip->answ = gone_good_old ? NULL : feo + 1;
@@ -604,7 +604,6 @@ fuse_vnop_fsync(struct vnop_fsync_args *ap)
     int           waitfor = ap->a_waitfor;
     vfs_context_t context = ap->a_context;
 
-    struct fuse_dispatcher  fdi;
     struct fuse_filehandle *fufh;
     struct fuse_vnode_data *fvdat = VTOFUD(vp);
 
@@ -644,11 +643,10 @@ fuse_vnop_fsync(struct vnop_fsync_args *ap)
         goto out;
     }
 
-    fdisp_init(&fdi, 0);
     for (type = 0; type < FUFH_MAXTYPE; type++) {
         fufh = &(fvdat->fufh[type]);
         if (FUFH_IS_VALID(fufh)) {
-            tmp_err = fuse_internal_fsync(vp, context, fufh, &fdi,
+            tmp_err = fuse_internal_fsync(vp, context, fufh,
                                           FUSE_OP_FOREGROUNDED);
             if (tmp_err) {
                 err = tmp_err;
@@ -745,6 +743,7 @@ fuse_vnop_getattr(struct vnop_getattr_args *ap)
     /* XXX: Could check the sanity/volatility of va_mode here. */
 
     if ((((struct fuse_attr_out *)fdi.answ)->attr.mode & S_IFMT) == 0) {
+        fuse_ticket_release(fdi.tick);
         return EIO;
     }
 
@@ -1559,7 +1558,6 @@ out:
                 fuse_internal_forget_send(vnode_mount(dvp), context,
                                           nodeid, 1, &fdi);
             }
-            return err;
         } else {
 
             if (!islastcn) {
@@ -2738,9 +2736,10 @@ fuse_vnop_reclaim(struct vnop_reclaim_args *ap)
 
     if ((!fuse_isdeadfs(vp)) && (fvdat->nlookup)) {
         struct fuse_dispatcher fdi;
-        fdi.tick = NULL;
+        fdisp_init(&fdi, 0);
         fuse_internal_forget_send(vnode_mount(vp), context, VTOI(vp),
                                   fvdat->nlookup, &fdi);
+        fuse_ticket_release(fdi.tick);
     }
 
     fuse_vncache_purge(vp);
@@ -3416,6 +3415,7 @@ fuse_vnop_symlink(struct vnop_symlink_args *ap)
 
     /* XXX: Need to take vap into account. */
 
+    /* Note: fuse_internal_newentry_core releases fdi.tick */
     err = fuse_internal_newentry_core(dvp, vpp, cnp, VLNK, &fdi, context);
 
     if (err == 0) {