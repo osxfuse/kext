osxfuse/kext

commit 0cebca926c37542735a838d4f150f0129a42dfd1

Rewrite function fuse_device_read
* Fix an issue, that can cause fuse_device_read to block although
  fuse_device_select signaled the the read would be non-blocking.

* fdata_set_dead is called by fuse_device_read when a FUSE_DESTROY message is
  sent to the FUSE server.

* No further messages are sent to the FUSE server after the file system has
  been destroyed by sending FUSE_DESTROY. Continuing to send messages might
  trigger a segmentation fault.

* Unmounting a FUSE volume does no longer signal VQ_DEAD. VQ_DEAD sould only be
  signaled in case the file system died unexpectedly.

fuse_device.c
@@ -293,13 +293,15 @@ int
 fuse_device_read(dev_t dev, uio_t uio, int ioflag)
 {
     int err = 0;
+    bool force = false;
     int i;
+
     size_t buflen[3];
     void *buf[] = { NULL, NULL, NULL };
-
+    
     struct fuse_device *fdev;
     struct fuse_data   *data;
-    struct fuse_ticket *ftick;
+    struct fuse_ticket *ftick = NULL;
 
     fuse_trace_printf_func();
 
@@ -310,94 +312,93 @@ fuse_device_read(dev_t dev, uio_t uio, int ioflag)
 
     data = fdev->data;
 
-again:
     fuse_lck_mtx_lock(data->ms_mtx);
 
-    /* The read loop (outgoing messages to the user daemon). */
+    /* The (non-)blocking read loop */
+    while (!ftick) {
+        if (fdata_dead_get(data)) {
+            err = ENODEV;
+        }
+        if (err) {
+            fuse_lck_mtx_unlock(data->ms_mtx);
+            return err;
+        }
 
-again_locked:
-    if (fdata_dead_get(data)) {
-        fuse_lck_mtx_unlock(data->ms_mtx);
-        return ENODEV;
+        ftick = fuse_ms_pop(data);
+        if (!ftick) {
+            if (ioflag & IO_NDELAY) {
+                err = EAGAIN;
+            } else {
+                err = fuse_msleep(data, data->ms_mtx, PCATCH, "fu_msg", NULL, data);
+            }
+        }
     }
 
-    if (!(ftick = fuse_ms_pop(data))) {
-        if (ioflag & IO_NDELAY) {
-            fuse_lck_mtx_unlock(data->ms_mtx);
-            return EAGAIN;
-        }
-
-        err = fuse_msleep(data, data->ms_mtx, PCATCH, "fu_msg", NULL, data);
-        if (err != 0) {
-            fuse_lck_mtx_unlock(data->ms_mtx);
-            return (fdata_dead_get(data) ? ENODEV : err);
-        }
-
-        goto again_locked;
+    if (fticket_opcode(ftick) == FUSE_DESTROY) {
+        /*
+         * The file system is in the process of being destroyed. We need to
+         * make sure no further messages are sent to the FUSE server, otherwise
+         * might crash due to a segementation fault.
+         */
+        force = fdata_set_dead(data);
     }
 
     fuse_lck_mtx_unlock(data->ms_mtx);
 
-    fuse_lck_mtx_lock(ftick->tk_aw_mtx);
-    if (fticket_answered(ftick)) {
-        fuse_remove_callback(ftick);
-
-        if (ftick->tk_interrupt) {
-            struct fuse_ticket *interrupt = ftick->tk_interrupt;
-
-            fuse_internal_interrupt_remove(interrupt);
-
-            /* Release interrupt ticket retained in fuse_internal_interrupt_send */
-            ftick->tk_interrupt = NULL;
-            fuse_ticket_release(interrupt);
-        }
-
-        fuse_lck_mtx_unlock(ftick->tk_aw_mtx);
-        fuse_ticket_release(ftick);
-
-        goto again;
-    }
-    fuse_lck_mtx_unlock(ftick->tk_aw_mtx);
-
-    if (fdata_dead_get(data)) {
-         if (ftick) {
-             fuse_ticket_release(ftick);
-         }
-         return ENODEV;
-    }
-
+    /* Handle different message types */
     switch (ftick->tk_ms_type) {
+        case FT_M_BUF:
+            buf[1]    = ftick->tk_ms_bufdata;
+            buflen[1] = ftick->tk_ms_bufsize;
 
-    case FT_M_FIOV:
-        buf[0]    = ftick->tk_ms_fiov.base;
-        buflen[0] = ftick->tk_ms_fiov.len;
-        break;
+        case FT_M_FIOV:
+            buf[0]    = ftick->tk_ms_fiov.base;
+            buflen[0] = ftick->tk_ms_fiov.len;
+            break;
 
-    case FT_M_BUF:
-        buf[0]    = ftick->tk_ms_fiov.base;
-        buflen[0] = ftick->tk_ms_fiov.len;
-        buf[1]    = ftick->tk_ms_bufdata;
-        buflen[1] = ftick->tk_ms_bufsize;
-        break;
-
-    default:
-        panic("OSXFUSE: unknown message type for ticket %p", ftick);
+        default:
+            panic("OSXFUSE: unknown message type %d for ticket %p", ftick->tk_ms_type, ftick);
     }
 
+    /* Transfer the ticket's data to user space */
     for (i = 0; buf[i]; i++) {
         if (uio_resid(uio) < (user_ssize_t)buflen[i]) {
-            data->dataflags |= FSESS_DEAD;
+            fdata_set_dead(data);
             err = ENODEV;
-            break;
+            goto out;
         }
-
         err = uiomove(buf[i], (int)buflen[i], uio);
-
         if (err) {
             break;
         }
     }
 
+    if (force) {
+        /* Send message without performing additional checks */
+        goto out;
+    }
+
+    /*
+     * Filter out tickets, that have been marked as answered by returning EINTR.
+     * In case this ticket has been interrupted drop the interrrupt ticket.
+     */
+    fuse_lck_mtx_lock(ftick->tk_aw_mtx);
+    if (fticket_answered(ftick)) {
+        fuse_remove_callback(ftick);
+        err = EINTR;
+
+        if (ftick->tk_interrupt) {
+            /* Set interrupt ticket to answered and remove its callback */
+            fuse_internal_interrupt_remove(ftick->tk_interrupt);
+        }
+    }
+    fuse_lck_mtx_unlock(ftick->tk_aw_mtx);
+
+    if (fdata_dead_get(data)) {
+        err = ENODEV;
+    }
+
+out:
     fuse_ticket_release(ftick);
     return err;
 }

fuse_internal.c
@@ -1627,11 +1627,14 @@ fuse_internal_interrupt_remove(struct fuse_ticket *interrupt)
     fuse_lck_mtx_lock(interrupt->tk_aw_mtx);
 
     /*
-     * Note: Pending requests that are already marked as answered will not
-     * not be sent to user space. See fuse_device_read().
+     * Set interrupt ticket state to answered and remove the callback. Pending
+     * requests, that are already marked as answered, will not be sent to user
+     * space.
+     *
+     * Note: Simply removing the ticket from the message queue would break
+     * fuse_device_select.
      */
     fticket_set_answered(interrupt);
-
     fuse_remove_callback(interrupt);
 
     fuse_lck_mtx_unlock(interrupt->tk_aw_mtx);

fuse_ipc.c
@@ -355,12 +355,12 @@ alreadydead:
             fuse_internal_interrupt_send(ftick);
         }
     }
-#endif
+#endif /* M_OSXFUSE_ENABLE_INTERRUPT */
 
 out:
     fuse_lck_mtx_unlock(ftick->tk_aw_mtx);
 
-    if (!(err || fticket_answered(ftick))) {
+    if (!err && !fticket_answered(ftick)) {
         IOLog("OSXFUSE: requester was woken up but still no answer");
         err = ENXIO;
     }

fuse_vfsops.c
@@ -729,6 +729,16 @@ fuse_vfsop_unmount(mount_t mp, int mntflags, vfs_context_t context)
     fuse_biglock_lock(data->biglock);
 #endif
 
+    /*
+     * Set mount state to FM_NOTMOUNTED.
+     *
+     * Note: fuse_device_read will call fdata_set_dead for us when sending the
+     * FUSE_DESTROY message. fdata_set_dead will signal VQ_DEAD if it is called
+     * for a volume, that is still mounted.
+     */
+    data->mount_state = FM_NOTMOUNTED;
+    OSAddAtomic(-1, (SInt32 *)&fuse_mount_count);
+
     if (!fdata_dead_get(data)) {
         fdisp_init(&fdi, 0 /* no data to send along */);
         fdisp_make(&fdi, FUSE_DESTROY, mp, FUSE_ROOT_ID, context);
@@ -738,18 +748,12 @@ fuse_vfsop_unmount(mount_t mp, int mntflags, vfs_context_t context)
             fuse_ticket_release(fdi.tick);
         }
 
-        /*
-         * Note that dounmount() signals a VQ_UNMOUNT VFS event.
-         */
-
-        fdata_set_dead(data);
+        /* Note that dounmount() signals a VQ_UNMOUNT VFS event */
     }
 
-    fuse_device_lock(fdev);
-
     vfs_setfsprivate(mp, NULL);
-    data->mount_state = FM_NOTMOUNTED;
-    OSAddAtomic(-1, (SInt32 *)&fuse_mount_count);
+
+    fuse_device_lock(fdev);
 
 #if M_OSXFUSE_ENABLE_INTERIM_FSNODE_LOCK && !M_OSXFUSE_ENABLE_HUGE_LOCK
     fuse_biglock_unlock(data->biglock);

commit 451cb58e2917104c868027f647374f8517aa215f

Fix unmount deadlock
The bug has been introduced in commit 0cebca9. Calling function fdata_set_dead
while data->ms_mtx is locked leads to a deadlock on Intel Macs or in case of
PPC Macs to a kernel panic.

fuse_device.c
@@ -298,7 +298,7 @@ fuse_device_read(dev_t dev, uio_t uio, int ioflag)
 
     size_t buflen[3];
     void *buf[] = { NULL, NULL, NULL };
-    
+
     struct fuse_device *fdev;
     struct fuse_data   *data;
     struct fuse_ticket *ftick = NULL;
@@ -312,9 +312,8 @@ fuse_device_read(dev_t dev, uio_t uio, int ioflag)
 
     data = fdev->data;
 
-    fuse_lck_mtx_lock(data->ms_mtx);
-
     /* The (non-)blocking read loop */
+    fuse_lck_mtx_lock(data->ms_mtx);
     while (!ftick) {
         if (fdata_dead_get(data)) {
             err = ENODEV;
@@ -333,6 +332,7 @@ fuse_device_read(dev_t dev, uio_t uio, int ioflag)
             }
         }
     }
+    fuse_lck_mtx_unlock(data->ms_mtx);
 
     if (fticket_opcode(ftick) == FUSE_DESTROY) {
         /*
@@ -342,8 +342,6 @@ fuse_device_read(dev_t dev, uio_t uio, int ioflag)
          */
         force = fdata_set_dead(data);
     }
-
-    fuse_lck_mtx_unlock(data->ms_mtx);
 
     /* Handle different message types */
     switch (ftick->tk_ms_type) {