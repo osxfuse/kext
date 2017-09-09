f0a59855c38eff8d87f827607ebda3014ba6766b
Benjamin Fleischer <fleiben@gmail.com>
2017-09-09 17:43:52 +0200

    Refactor vnode notification code
    * We only need to fill in specific vnode attributes. For details see
      VNODE_NOTIFY_ATTRS in bsd/sys/vnode.h.
    
    * Calling fuse_internal_attr_loadvap() will result in double-posted
      events.

--- osxfuse/fuse_notify.c
+++ osxfuse/fuse_notify.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2016 Benjamin Fleischer
+ * Copyright (c) 2012-2017 Benjamin Fleischer
  * All rights reserved.
  */
 
@@ -11,39 +11,36 @@
 #include "fuse_knote.h"
 #include "fuse_node.h"
 
-#include <libkern/version.h>
 #include <sys/ubc.h>
 
-#if M_OSXFUSE_ENABLE_INTERIM_FSNODE_LOCK
+int
+fuse_vnode_notify(vnode_t vp, uint32_t events)
+{
+#if M_OSXFUSE_ENABLE_VNODE_NOTIFY
+    struct vnode_attr va;
+    struct vnode_attr *vap = &va;
+    struct vnode_attr *cached_vap = VTOVA(vp);
+    
+    (void)vfs_get_notify_attributes(vap);
+    
+    VATTR_RETURN(vap, va_fsid, cached_vap->va_fsid);
+    VATTR_RETURN(vap, va_fileid, cached_vap->va_fileid);
+    VATTR_RETURN(vap, va_mode, cached_vap->va_mode);
+    VATTR_RETURN(vap, va_uid, cached_vap->va_uid);
+    VATTR_RETURN(vap, va_gid, cached_vap->va_gid);
+    VATTR_RETURN(vap, va_nlink, cached_vap->va_nlink);
+    
+    return vnode_notify(vp, events, vap);
+    
+#else /* M_OSXFUSE_ENABLE_VNODE_NOTIFY */
+    (void)vp;
+    (void)events;
 
-static
-void
-fuse_vnotify(vnode_t vp) {
-    // Those flag constants are not available in
-    // headers, so we need to copy them here.
-    static const int VNODE_EVENT_WRITE  = 0x2;
-    static const int VNODE_EVENT_ATTRIB = 0x8;
-
-#if VERSION_MAJOR >= 15
-    struct vnode_attr vattr;
-    vfs_get_notify_attributes(&vattr);
-
-#if M_OSXFUSE_ENABLE_BIG_LOCK
-    struct fuse_data *data = fuse_get_mpdata(vnode_mount(vp));
-    fuse_biglock_lock(data->biglock);
-#endif
-
-    fuse_internal_attr_loadvap(vp, &vattr, NULL);
-
-#if M_OSXFUSE_ENABLE_BIG_LOCK
-    fuse_biglock_unlock(data->biglock);
-#endif
-
-    vnode_notify(vp, VNODE_EVENT_ATTRIB | VNODE_EVENT_WRITE, &vattr);
-#else
-    (void) vp; // Unused parameter.
-#endif
+    return 0;
+#endif /* M_OSXFUSE_ENABLE_VNODE_NOTIFY */
 }
+
+#if M_OSXFUSE_ENABLE_INTERIM_FSNODE_LOCK
 
 static
 void
@@ -55,22 +52,27 @@ fuse_notify_getattr(void *parameter, __unused wait_result_t wait_result)
     struct fuse_vnode_data *fvdat = VTOFUD(vp);
     struct fuse_data *data = fuse_get_mpdata(vnode_mount(vp));
 
+    vfs_context_t context;
+    
     struct fuse_dispatcher fdi;
     struct fuse_abi_data fgi;
     struct fuse_abi_data fao;
     struct fuse_abi_data fa;
 
+    uint32_t events = 0;
     off_t old_filesize;
     off_t new_filesize;
 
     fuse_biglock_lock(data->biglock);
+
+    context = vfs_context_create(NULL);
 
     if (fuse_isdeadfs(vp)) {
         goto out;
     }
 
     fdisp_init_abi(&fdi, fuse_getattr_in, data);
-    fdisp_make_vp(&fdi, FUSE_GETATTR, vp, vfs_context_current());
+    fdisp_make_vp(&fdi, FUSE_GETATTR, vp, context);
     fuse_abi_data_init(&fgi, DATOI(data), fdi.indata);
 
     fuse_getattr_in_set_fh(&fgi, 0);
@@ -79,8 +81,10 @@ fuse_notify_getattr(void *parameter, __unused wait_result_t wait_result)
     err = fdisp_wait_answ(&fdi);
     if (err) {
         if (err == ENOENT) {
+            events |= FUSE_VNODE_EVENT_DELETE;
+            
             fuse_biglock_unlock(data->biglock);
-            fuse_internal_vnode_disappear(vp, vfs_context_current(), REVOKE_SOFT);
+            fuse_internal_vnode_disappear(vp, context, REVOKE_SOFT);
             fuse_biglock_lock(data->biglock);
         }
         goto out;
@@ -89,6 +93,12 @@ fuse_notify_getattr(void *parameter, __unused wait_result_t wait_result)
     fuse_abi_data_init(&fao, DATOI(data), fdi.answ);
     fuse_abi_data_init(&fa, fao.fad_version, fuse_attr_out_get_attr(&fao));
 
+    /*
+     * Note: Actually we don't know if file attributes or contents have changed
+     * but we send out the notification anyway.
+     */
+    events = FUSE_VNODE_EVENT_ATTRIB | FUSE_VNODE_EVENT_WRITE;
+    
     cache_attrs(vp, fuse_attr_out, &fao);
 
     old_filesize = fvdat->filesize;
@@ -102,15 +112,19 @@ fuse_notify_getattr(void *parameter, __unused wait_result_t wait_result)
         ubc_setsize(vp, new_filesize);
 
         if (new_filesize > old_filesize) {
+            events |= FUSE_VNODE_EVENT_EXTEND;
+            
             /*
-             * Note: Unless the file did end on a page boundary we need to invalidate the
-             * last page of the file's unified buffer cache maunally. ubc_setsize does not
-             * take care of this when expanding files.
+             * Note: Unless the file did end on a page boundary we need to
+             * invalidate the last page of the file's unified buffer cache
+             * maunally. ubc_setsize does not take care of this when expanding
+             * files.
              */
 
             off_t end_off = round_page_64(old_filesize);
             if (end_off != old_filesize) {
-                ubc_msync(vp, trunc_page_64(old_filesize), end_off, NULL, UBC_INVALIDATE);
+                ubc_msync(vp, trunc_page_64(old_filesize), end_off, NULL,
+                          UBC_INVALIDATE);
             }
         }
 
@@ -118,8 +132,10 @@ fuse_notify_getattr(void *parameter, __unused wait_result_t wait_result)
     }
 
 out:
-    FUSE_KNOTE(vp, NOTE_ATTRIB);
-    fuse_vnotify(vp);
+    FUSE_KNOTE(vp, events);
+    fuse_vnode_notify(vp, events);
+
+    vfs_context_rele(context);
 
     /*
      * Note: We need to unlock the node and decrement the vnode's iocount. See
@@ -143,6 +159,7 @@ fuse_notify_inval_entry(struct fuse_data *data, struct fuse_iov *iov)
     void *next;
 
     uint32_t namelen;
+    uint64_t parent_ino;
 
     HNodeRef dhp;
     vnode_t dvp;
@@ -162,11 +179,9 @@ fuse_notify_inval_entry(struct fuse_data *data, struct fuse_iov *iov)
     memcpy(name, next, namelen);
     name[namelen] = '\0';
 
-    err = (int)HNodeLookupRealQuickIfExists(data->fdev,
-                                            (ino_t)fuse_notify_inval_entry_out_get_parent(&fnieo),
-                                            0 /* fork index */,
-                                            &dhp,
-                                            &dvp);
+    parent_ino = fuse_notify_inval_entry_out_get_parent(&fnieo);
+    err = (int)HNodeLookupRealQuickIfExists(data->fdev, parent_ino,
+                                            0 /* fork index */, &dhp, &dvp);
     if (err) {
         return err;
     }
@@ -176,7 +191,7 @@ fuse_notify_inval_entry(struct fuse_data *data, struct fuse_iov *iov)
      * We have to look up the vnode for the specified name in the vnode cache,
      * to purge it from the cache.
      *
-     * Note: Without flag MAKEENTRY cache_lookup does not return the vnode.
+     * Note: Without flag MAKEENTRY cache_lookup() does not return the vnode.
      */
 
     memset(&cn, 0, sizeof(cn));
@@ -205,7 +220,7 @@ fuse_notify_inval_entry(struct fuse_data *data, struct fuse_iov *iov)
 
     fuse_invalidate_attr(dvp);
     FUSE_KNOTE(dvp, NOTE_ATTRIB);
-    fuse_vnotify(dvp);
+    fuse_vnode_notify(dvp, FUSE_VNODE_EVENT_ATTRIB);
 
 out:
     fuse_nodelock_unlock(VTOFUD(dvp));
@@ -233,9 +248,9 @@ fuse_notify_inval_inode(struct fuse_data *data, struct fuse_iov *iov)
 
     if (!vfs_issynchronous(data->mp)) {
         /*
-         * Enabling asynchronous writes for distributed file systems is not supported.
-         * Files might end up in an inconsitent state. Therefore we do the only sane
-         * thing and ignore this notification.
+         * Enabling asynchronous writes for distributed file systems is not
+         * supported. Files might end up in an inconsitent state. Therefore we
+         * do the only sane thing and ignore this notification.
          */
         err = ENOSYS;
         goto out;
@@ -247,7 +262,8 @@ fuse_notify_inval_inode(struct fuse_data *data, struct fuse_iov *iov)
     off = fuse_notify_inval_inode_out_get_off(&fniio);
     len = fuse_notify_inval_inode_out_get_len(&fniio);
 
-    err = (int)HNodeLookupRealQuickIfExists(data->fdev, ino, 0 /* fork index */, &hp, &vp);
+    err = (int)HNodeLookupRealQuickIfExists(data->fdev, ino, 0 /* fork index */,
+                                            &hp, &vp);
     if (err) {
         return err;
     }
@@ -278,8 +294,8 @@ fuse_notify_inval_inode(struct fuse_data *data, struct fuse_iov *iov)
 
         if (kr == KERN_SUCCESS) {
             /*
-             * Note: We will unlock the node and derement the vnode's iocount after updating
-             * the file's attributes in fuse_notify_getattr.
+             * Note: We will unlock the node and derement the vnode's iocount
+             * after updating the file's attributes in fuse_notify_getattr.
              */
 
             thread_deallocate(getattr_thread);
@@ -291,8 +307,9 @@ fuse_notify_inval_inode(struct fuse_data *data, struct fuse_iov *iov)
         }
     }
 
-    FUSE_KNOTE(vp, NOTE_ATTRIB);
-    fuse_vnotify(vp);
+    FUSE_KNOTE(vp, NOTE_ATTRIB | NOTE_WRITE);
+    fuse_vnode_notify(vp, FUSE_VNODE_EVENT_ATTRIB | FUSE_VNODE_EVENT_WRITE);
+
     fuse_nodelock_unlock(VTOFUD(vp));
     vnode_put(vp);
 
--- osxfuse/fuse_notify.h
+++ osxfuse/fuse_notify.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Benjamin Fleischer
+ * Copyright (c) 2012-2017 Benjamin Fleischer
  * All rights reserved.
  */
 
@@ -9,6 +9,61 @@
 #include "fuse.h"
 
 #include "fuse_ipc.h"
+
+#include <fuse_param.h>
+
+/*
+ * Note: The following vnode event flags are kernel-private, therefore we need
+ * to define them here. The flags need to be kept in sync with those defined in
+ * bsd/sys/vnode.h.
+ *
+ * FUSE_VNODE_EVENT_DELETE
+ *     file was removed
+ *
+ * FUSE_VNODE_EVENT_WRITE
+ *     file or directory contents changed
+ *
+ * FUSE_VNODE_EVENT_EXTEND
+ *     ubc size increased
+ *
+ * FUSE_VNODE_EVENT_ATTRIB
+ *     attributes changed (suitable for permission changes if type unknown)
+ *
+ * FUSE_VNODE_EVENT_LINK
+ *     link count changed
+ *
+ * FUSE_VNODE_EVENT_RENAME
+ *     vnode was renamed
+ *
+ * FUSE_VNODE_EVENT_PERMS
+ *     permissions changed: will cause a NOTE_ATTRIB
+ *
+ * FUSE_VNODE_EVENT_FILE_CREATED
+ *     file created in directory, will cause NOTE_WRITE
+ *
+ * FUSE_VNODE_EVENT_DIR_CREATED
+ *     directory created inside this directory, will cause NOTE_WRITE
+ *
+ * FUSE_VNODE_EVENT_FILE_REMOVED
+ *     file removed from this directory, will cause NOTE_WRITE
+ *
+ * FUSE_VNODE_EVENT_DIR_REMOVED
+ *     subdirectory from this directory, will cause NOTE_WRITE
+ */
+
+#define FUSE_VNODE_EVENT_DELETE         0x00000001
+#define FUSE_VNODE_EVENT_WRITE          0x00000002
+#define FUSE_VNODE_EVENT_EXTEND         0x00000004
+#define FUSE_VNODE_EVENT_ATTRIB         0x00000008
+#define FUSE_VNODE_EVENT_LINK           0x00000010
+#define FUSE_VNODE_EVENT_RENAME         0x00000020
+#define FUSE_VNODE_EVENT_PERMS          0x00000040
+#define FUSE_VNODE_EVENT_FILE_CREATED   0x00000080
+#define FUSE_VNODE_EVENT_DIR_CREATED    0x00000100
+#define FUSE_VNODE_EVENT_FILE_REMOVED   0x00000200
+#define FUSE_VNODE_EVENT_DIR_REMOVED    0x00000400
+
+int fuse_vnode_notify(vnode_t vp, uint32_t events);
 
 #if M_OSXFUSE_ENABLE_INTERIM_FSNODE_LOCK
 
