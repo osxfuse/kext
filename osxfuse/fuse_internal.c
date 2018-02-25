d5e4099ba108bf1f8813883b9251f8dfc948cdd4
Benjamin Fleischer <fleiben@gmail.com>
2018-02-25 23:08:11 +0100

    Fix file handle bug in exchangedata(2)
    Calling vnode_update_identity() in fuse_internal_exchange() to swap
    vnode names and parents is unnecessary. exchangedata(2) already takes
    care of this for us.
    
    In fact, calling vnode_update_identity() results in open file handles
    pointing to the wrong file after calling exchangedata(2).
    
    Closes osxfuse/osxfuse#474

--- osxfuse/fuse_internal.c
+++ osxfuse/fuse_internal.c
@@ -269,17 +269,6 @@ fuse_internal_exchange(vnode_t       fvp,
 #endif
 
         /*
-         * We need to increase the iocount of fdvp to make sure it will not be reclaimed
-         * when assiginig fvp a new parent.
-         */
-        vnode_get(fdvp);
-
-        vnode_update_identity(fvp, tdvp, tname, (int)tlen, 0, VNODE_UPDATE_PARENT | VNODE_UPDATE_NAME);
-        vnode_update_identity(tvp, fdvp, fname, (int)flen, 0, VNODE_UPDATE_PARENT | VNODE_UPDATE_NAME);
-
-        vnode_put(fdvp);
-
-        /*
          * Another approach (will need additional kernel support to work):
          *
          * vnode_t tmpvp = ffud->vp;
